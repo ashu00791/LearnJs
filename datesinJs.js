c30dcf9767c4df2954cb7550ddd055945fefc3c5
ashu00791 <101887151+ashu00791@users.noreply.github.com>
2023-08-29 10:08:23 +0000

    datesinjs done

---
+++ datesinJs.js
@@ -0,0 +1,18 @@
+
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 9, 14)
+// console.log(myCreatedDate.toString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
